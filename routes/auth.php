bf67b1b7826eb350a70d4eabf35a429158ba75ce
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-03-02 11:55:26 +0530

    My Layout Part Completed Except Trainer Registration page

--- routes/auth.php
+++ routes/auth.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/login', 'login')->name('login');
 Route::view('/signup', 'signup')->name('signup');
+Route::view('/register', 'register')->name('register');
 
