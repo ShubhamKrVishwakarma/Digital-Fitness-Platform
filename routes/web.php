dad4a1e03b862002b4890f657415e1cebb5b5bd8
DhanrajDwivedi <dhanrajdwivedi2003@gmail.com>
2024-03-02 16:35:13 +0530

    Layoust done for orders page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/orders', function () {
+    return view('order');
+})->name('order');
