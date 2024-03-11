a0663deffd9f3b37292f228ba3c03ebd976264d9
Roshan <rs8013725173@gmail.com>
2024-03-11 19:49:33 +0530

    fixed a bug

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon.png') }}">
     <title>Fitness Hub - @yield("title")</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     @stack("styles")
 </head>
