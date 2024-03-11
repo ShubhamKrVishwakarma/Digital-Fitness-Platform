1c4ea2f8b7f8365f539e2325a540369d56cdcc3d
Sayan <sayansom48@gmail.com>
2024-03-11 19:33:30 +0530

    Query of Contact Page Done

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Query;
+use Exception;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class QueryController extends Controller
 {
-    //
+    public function index(){
+        return view("contact");
+    }
+
+    public function store(Request $request){
+        // dd($request->all());
+        $request->validate([
+            "name" => "required|min:2|max:255",
+            "phone" => "required|min:10",
+            "email" => "required|email",
+            "message" => "required|max:250"
+        ]);
+        Query::create([
+            "name" => $request->name,
+            "phone" => $request->phone,
+            "email" => $request->email,
+            "message" => $request->message
+        ]);
+        return redirect()->route("contact");
+    }
 }
