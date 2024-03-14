15a6e63febe8b7fe022870f20ee4d33dfecfe42b
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-03-14 18:07:47 +0530

    Updated Member seeder added additional column data

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -12,14 +12,22 @@ class MemberSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i=1 ; $i<10 ; $i++) {
-            User::create([
+        $userData = [];
+
+        for ($i = 1; $i < 20; $i++) {
+            $userData[] = [
                 "name" => fake()->name(),
                 "email" => fake()->unique()->email(),
-                "password" => "ssssssss",
-                "gender" => fake()->randomElement(['M', 'F']),
-                "dob" => fake()->date()
-            ]);
+                "phone" => fake()->numerify('##########'),
+                "password" => password_hash("ssssssss", PASSWORD_DEFAULT),
+                "gender" => fake()->randomElement(['M', 'F', 'O']),
+                "dob" => fake()->date(),
+                "address" => fake()->address(),
+                "city" => fake()->city(),
+                "zip_code" => fake()->countryCode()
+            ];
         }
+
+        User::insert($userData);
     }
 }
