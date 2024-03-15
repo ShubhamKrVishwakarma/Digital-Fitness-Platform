4c3bf483934e090c3124097171b3b29b6a6e2847
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-03-15 13:56:35 +0530

    Added Query Seeder (Done By Sayan)

---
+++ database/seeders/QuerySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Query;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class QuerySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $query=[];
+        for($i=1;$i<=100;$i++){
+            $query[] = [
+                "name"=>fake()->name(),
+                "email"=>fake()->email(),
+                "phone" => fake()->numerify('##########'),
+                "message"=> fake()->text(),
+                "created_at"=> now(),
+                "updated_at"=> now()
+            ];
+        }
+        Query::insert($query);
+    }
+}
