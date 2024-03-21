bf3ebd749f6d20094945fc2ea6b4b3de22e6289d
Roshan <rs8013725173@gmail.com>
2024-03-21 18:33:56 +0530

    added some more columns in orders table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id' ,'user_role', 'status' ,'total_price'];
+    protected $fillable = ['user_id' ,'user_role', 'status' ,'total_price', 'address', 'phone', 'state', 'city', 'zip_code' ];
 
     public function user() {
         return $this->belongsTo(User::class, 'user_id', 'id');
