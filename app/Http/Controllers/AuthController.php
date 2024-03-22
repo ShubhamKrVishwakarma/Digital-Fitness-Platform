e1137763e3f16046e5a32595dab385fbf8d7a96f
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-03-22 20:39:01 +0530

    Changed Member Signup Page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Date;
+
 class AuthController extends Controller
 {
     public function login()
@@ -17,6 +23,43 @@ class AuthController extends Controller
     public function register()
     {
         return view('Auth.register');
+    }
+
+    public function authenticate() {
+
+    }
+
+    public function create(Request $request) {
+        try {
+            $validator = Validator::make($request->all(), [
+                "name" => "required|min:2|max:100",
+                "email" => "required|email|min:5|max:100|unique:users,email",
+                "gender" => "required|in:M,F,O",
+                "date_of_birth" => [
+                    "required",
+                    "date",
+                    "before:" . Date::now()->subYears(15)->format('Y-m-d'),
+                ],
+                "password" => "required",
+                "confirm_password" => "required|same:password"
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+
+            User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "gender" => $request->gender,
+                "dob" => $request->date_of_birth,
+                "password" => $request->password
+            ]);
+
+            return response()->json(['message' => 'Account created successfully']);
+        } catch (Exception) {
+            return response()->json(['error' => 'Server Error'], 500);
+        }
     }
 
     public function logout() {
---
+++ public/js/auth.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    // Member Signup
+    $('#signupForm').submit(function(form) {
+        form.preventDefault();
+        
+        axios.post("/signup", {
+            name: $('#signup-name').val(),
+            email: $('#signup-email').val(),
+            gender: $('#signup-gender').val(),
+            date_of_birth: $('#signup-dob').val(),
+            password: $('#signup-password').val(),
+            confirm_password: $('#signup-confirm-password').val(),
+        })
+        .then(function(response) {
+            $('.text-danger').text('');
+            $('#signup-name').val('');
+            $('#signup-email').val('');
+            $('#signup-gender').val('');
+            $('#signup-dob').val('');
+            $('#signup-password').val('');
+            $('#signup-confirm-password').val('');
+
+            setTimeout(()=> {
+                window.location.href = "http://127.0.0.1:8000/";
+            }, 1500);
+
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Account Created Successfully",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        })
+        .catch(function(error) {
+            if (error.response && error.response.status === 422) {
+                signupValidationsErrors(error.response.data.errors);
+            } else {
+                Swal.fire({
+                    title: "Server Error",
+                    text: "Something Went Wrong!",
+                    icon: "error"
+                });
+            }
+        });
+    });
+
+    // Member Signup Validations Error
+    function signupValidationsErrors(errors) {
+        $('.text-danger').text('');
+
+        if (errors.name) {
+            $('#signup-name-error').text(errors.name[0]);
+        }
+
+        if (errors.email) {
+            $('#signup-email-error').text(errors.email[0]);
+        }
+
+        if (errors.gender) {
+            $('#signup-gender-error').text(errors.gender[0]);
+        }
+
+        if (errors.date_of_birth) {
+            if (errors.date_of_birth[0] === "The date of birth field must be a date before 2009-03-22.") {
+                $('#signup-dob-error').text('You should be at least 15 years old!');
+            } else {
+                $('#signup-dob-error').text(errors.date_of_birth[0]);
+            }
+        }
+
+        if (errors.password) {
+            $('#signup-password-error').text(errors.password[0]);
+        }
+        
+        if (errors.confirm_password) {
+            $('#signup-confirm-password-error').text(errors.confirm_password[0]);
+        }
+    }
+});
