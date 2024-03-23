d128288bb3e17ac0111c08c20c016902e61eac9f
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-03-23 13:33:31 +0530

    Added Order Details Page and Changed Some CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,24 @@
-@extends("layouts.mainLayout")
+@extends('layouts.mainLayout')
 
-@section("title", "Home")
+@section('title', 'Home')
 
-@push("styles")
+@push('styles')
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 @endpush
 
-@section("content")
-    <section class="container-fluid banner text-white" id="banner-1">
+@section('content')
+    <section class="container-fluid banner" id="banner-1">
         <video autoplay muted loop plays-inline class="background-clip">
-          <source src="{{asset("videos/fitness_video.webm")}}" type="video/webm">
+            <source src="{{ asset('videos/fitness_video.webm') }}" type="video/webm">
         </video>
         <div class="content">
             <h4 class="mb-3" data-text="FITNESS_HUB">FITNESS_HUB</h4>
-            <h1 class="mb-0 pb-0"><span>MAKE</span> YOUR</h1> 
+            <h1 class="mb-0 pb-0"><span>MAKE</span> YOUR</h1>
             <h1 class="mb-5">BODY SHAPE</h1>
             <p>
-              Fitness that Sticks
-              Get started with out Personalized Workouts
+                Fitness that Sticks
+                Get started with out Personalized Workouts
             </p>
             <div class="banner-button mt-4">
                 <button class="bttn bttn-primary">Get Started</button>
@@ -26,10 +26,10 @@
         </div>
     </section>
 
-    <section class="container-fluid banner animate text-white" id="banner-2">
+    <section class="container-fluid banner animate" id="banner-2">
         <div class="heading">
             <h2>Start Your Fitness Journey.</h2>
-            <h4>With our Pre-Planned Workouts</h4>
+            <h4 class="text-dark">With our Pre-Planned Workouts</h4>
         </div>
         <div class="content animate">
             <div class="content-1 animate-2">
@@ -63,22 +63,18 @@
     </section>
 
     <section class="container-fluid banner animate" id="banner-3">
-      <div class="image animate">
-        <img src="./images/home/bar.jpg" alt="">
-      </div>
-      <div class="content">
-          <h1 class=" mb-5 display-2">SHOP VARIETY OF <br>EQUIPMENTS</h1>
-          <!-- <p>
-              We are offering variety of gym equipments as well as suppliments to boost your fitness
-              journey.
-          </p> -->
-          <div class="banner-button">
-              <button class="bttn bttn-primary">Go to Store</button>
-          </div>
-      </div>
+        <div class="image animate">
+            <img src="./images/home/bar.jpg" alt="">
+        </div>
+        <div class="content">
+            <h1 class=" mb-5 display-2">SHOP VARIETY OF <br>EQUIPMENTS</h1>
+            <div class="banner-button">
+                <button class="bttn bttn-primary">Go to Store</button>
+            </div>
+        </div>
     </section>
 
-    <section class="container-fluid banner animate text-white" id="banner-4">
+    <section class="container-fluid banner animate" id="banner-4">
         <div class="heading pb-sm-5">
             <h2>Our Top rated Trainers</h2>
         </div>
@@ -89,7 +85,7 @@
                 <p>
                     Checkout profile.
                 </p>
-                
+
                 <a href="#"><i class="bi bi-box-arrow-right"></i></a>
                 {{-- <a href="{{ route('user.show'),['id'=>user->id]}}"><i class="bi bi-box-arrow-right"></i></a> --}}
             </div>
@@ -118,87 +114,94 @@
         </div>
     </section>
 
-    <section class="container-fluid banner animate text-white" id="banner-5">
+    <section class="container-fluid banner animate" id="banner-5">
         <div class="image">
             <img src="./images/home/community2.jpg" alt="">
         </div>
         <div class="content">
             <div>
                 <h1 class="mb-5 display-2">Explore <br>our Community hub</h1>
-                <!-- <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium debitis at nulla ex? Totam
-                    veritatis cumque nihil id pariatur amet dolorem consequatur, inventore quibusdam officiis
-                    voluptatibus officia asperiores provident possimus?0</p> -->
             </div>
             <div class="banner-button">
                 <button class="bttn bttn-primary">Explore</button>
             </div>
         </div>
     </section>
-    
+
     <section class="container-fluid animate text-dark" id="banner-6">
         <div class="heading">
-          <h4 class="text-center">Trusted by
-            <span class="fw-bold text-dark">20,000+</span>
-            organizations worldwide
-          </h4>
+            <h4 class="text-center">Trusted by
+                <span class="fw-bold text-dark">20,000+</span>
+                organizations worldwide
+            </h4>
         </div>
         <div class="slider">
-          <div class="slide-track">
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-1.svg" alt="logo" />
-              </figure>
+            <div class="slide-track">
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-1.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-2.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-3.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-4.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-5.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-1.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-2.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-3.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-4.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
+                <div class="slide">
+                    <figure class="text-center">
+                        <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-5.svg"
+                            alt="logo" />
+                    </figure>
+                </div>
             </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-2.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-3.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-4.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-5.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-1.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-2.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-3.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-4.svg" alt="logo" />
-              </figure>
-            </div>
-            <div class="slide">
-              <figure class="text-center">
-                <img src="https://block.codescandy.com/assets/images/client-logo/clients-logo-5.svg" alt="logo" />
-              </figure>
-            </div>
-          </div>
         </div>
-      </section>
+    </section>
 @endsection
 
-@push("scripts")
-<script src="{{ asset('js/home.js') }}"></script>
+@push('scripts')
+    <script src="{{ asset('js/home.js') }}"></script>
 @endpush
