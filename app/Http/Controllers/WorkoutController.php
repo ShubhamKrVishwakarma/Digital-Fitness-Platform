1c6da5d358d29ad9b7468df0f807064f6de0ae57
Roshan <rs8013725173@gmail.com>
2024-03-26 23:00:43 +0530

    added workout plan and workout page with some functionalities

---
+++ app/Http/Controllers/WorkoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Workout;
+use Illuminate\Http\Request;
+
+class WorkoutController extends Controller
+{
+    public function index($plan_id){
+        return view('workout',['workouts'=>Workout::where('plan_id' , $plan_id )->get()]);
+    }
+}
