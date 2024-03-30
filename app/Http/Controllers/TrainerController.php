7b37bef5d1fefd70388dc3131c58b77a7134aa2b
DhanrajDwivedi <dhanrajdwivedi2003@gmail.com>
2024-03-30 23:31:55 +0530

    Reviews and rating of trainer

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Review;
+use App\Models\ReviewType;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,12 +16,20 @@ class TrainerController extends Controller
         ]);
     }
 
-    // public function rate(Request $request){
-    //     $request->validate([
-    //         "trainer-rating"=> "required|integer"
-    //     ]);
-    //     Review::create([
-    //         ""
-    //     ])
-    // }
+    public function rate(Request $request){
+        $request->validate([
+            "trainer-rating"=> "required"
+        ]);
+        $rating = Review::create([
+            "user_id" => auth()->user()->id,
+            "rating" => $request["trainer-rating"],
+            "review" => $request["trainer-review"],
+        ]);
+        ReviewType::create([
+            "review_id" => $rating->id,
+            "type" => "trainer",
+            "trainer_id" =>  $request["trainer-id"],
+        ]);
+        return redirect()->route("trainers");
+    }
 }
