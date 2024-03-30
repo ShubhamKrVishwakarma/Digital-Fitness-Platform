4cf1ea17970663f8f84b0639daed4cb8e638212b
Roshan <rs8013725173@gmail.com>
2024-03-30 19:54:31 +0530

    added review and review type tables

---
+++ app/Models/ReviewType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReviewType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['review_id', 'trainer_id','product_id','type'];
+
+    public function trainer(){
+        return $this->belongsTo(User::class,'trainer_id' , 'id');
+    }
+    public function product(){
+        return $this->belongsTo(Product::class,'product_id' , 'id');
+    }
+}
