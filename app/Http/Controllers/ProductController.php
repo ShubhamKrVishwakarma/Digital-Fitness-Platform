8ccad118d1eee6cd99810d51b73b7b96c99a3ad7
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-03-31 10:14:36 +0530

    Avoid this Checkpiont

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,23 +8,5 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-        return view('shop' , [
-            "products" => Product::all()
-        ]);
-    }
     
-    public function addToCart($id){
-        $cart=Cart::where("product_id", $id)->first();
-        if (!$cart){
-            $product=Product::find($id);
-            Cart::create([
-                "user_id"=>auth()->user()->id,
-                "product_id"=>$id,
-                "quantity"=>1,
-                "price"=>$product->price
-            ]);
-        }
-        return redirect()->route('cart');
-    }
 }
