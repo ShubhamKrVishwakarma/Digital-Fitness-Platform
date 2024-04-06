e37231f7893881fb48a23c1764fdd6397ae6c765
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-06 05:48:56 +0530

    Added Community Info to Admin Community PAge

---
+++ resources/views/livewire/admin/community/community-info.blade.php
@@ -0,0 +1,105 @@
+<div class="row justify-content-center align-items-center">
+    <div class="col-xl-4 col-sm-8 mb-xl-0 mb-4">
+        <div class="card">
+            <div class="card-body p-3">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Posts</p>
+                            <h5 class="font-weight-bolder">
+                                {{ $total_posts }}
+                            </h5>
+                            <p class="mb-0">
+                                <span class="text-success text-sm font-weight-bolder">+55%</span>
+                                since yesterday
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div
+                            class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                            <i class="fa-solid fa-rss text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-4 col-sm-8 mb-xl-0 mb-4">
+        <div class="card">
+            <div class="card-body p-3">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-uppercase font-weight-bold">Todal Likes</p>
+                            <h5 class="font-weight-bolder">
+                                {{ $total_likes }}
+                            </h5>
+                            <p class="mb-0">
+                                <span class="text-success text-sm font-weight-bolder">+3%</span>
+                                since last week
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div
+                            class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
+                            <i class="fa-solid fa-heart text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-4 col-sm-8 mb-xl-0 mb-4">
+        <div class="card">
+            <div class="card-body p-3">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Comments</p>
+                            <h5 class="font-weight-bolder">
+                                {{ $total_comments }}
+                            </h5>
+                            <p class="mb-0">
+                                <span class="text-danger text-sm font-weight-bolder">+2%</span>
+                                than last month
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div
+                            class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                            <i class="fa-solid fa-comment text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="col-xl-3 col-sm-6">
+        <div class="card">
+            <div class="card-body p-3">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="numbers">
+                            <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Shares</p>
+                            <h5 class="font-weight-bolder">
+                                34
+                            </h5>
+                            <p class="mb-0">
+                                <span class="text-success text-sm font-weight-bolder">+5%</span> than last month
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-4 text-end">
+                        <div
+                            class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
+                            <i class="fa-solid fa-share-nodes text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+</div>
