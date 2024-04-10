ecc4b811773f7c0677feb2bb7eb62d9914de30b1
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-10 12:40:02 +0530

    Profile Image Fix in Admins User Page

--- resources/views/livewire/admin/user/manage-user.blade.php
+++ resources/views/livewire/admin/user/manage-user.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-auto">
                     @if ($profile_pic)
                         <div class="avatar avatar-xl position-relative">
-                            <img src="{{ url('storage/user') . '/' . $profile_pic }}" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+                            <img src="{{ $profile_pic }}" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
                         </div>
                     @else
                         <div class="avatar avatar-xl position-relative">
@@ -198,7 +198,7 @@
                                 <div class="mt-n4 mt-lg-n6 mb-4 mb-lg-0">
                                     @if ($profile_pic)
                                         <a>
-                                            <img src="{{ url('storage/user') . '/' . $profile_pic }}"
+                                            <img src="{{ $profile_pic }}"
                                                 class="rounded-circle img-fluid border border-2 border-white">
                                         </a>
                                     @else
