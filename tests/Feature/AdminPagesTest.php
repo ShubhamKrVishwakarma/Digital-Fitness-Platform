7a404c32355e514be5e35bf8cca7caf9c974a328
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-15 20:14:31 +0530

    Added More Admin Pages Test

--- tests/Feature/AdminPagesTest.php
+++ tests/Feature/AdminPagesTest.php
@@ -22,7 +22,7 @@ class AdminPagesTest extends TestCase
         ]);
     }
     
-    public function test_member_cannot_access_admin_dashboard(): void
+    public function test_unauthorized_user_cannot_access_admin_dashboard(): void
     {
         $user = $this->user();
 
@@ -31,11 +31,92 @@ class AdminPagesTest extends TestCase
         $response->assertStatus(403);
     }
 
-    public function test_only_admin_can_access_admin_dashboard(): void
+    public function test_only_admin_can_access_admin_dashboard_page(): void
     {
         $user = $this->user("admin");
 
         $response = $this->actingAs($user)->get('/Admin/dashboard');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_users_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/users');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_exercises_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/exercises');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_workout_plan_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/workout_plan');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_categories_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/categories');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_products_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/products');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_conversations_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/products');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_community_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/products');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_orders_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/orders');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_only_admin_can_access_admin_queries_page(): void
+    {
+        $user = $this->user("admin");
+
+        $response = $this->actingAs($user)->get('/Admin/queries');
 
         $response->assertStatus(200);
     }
