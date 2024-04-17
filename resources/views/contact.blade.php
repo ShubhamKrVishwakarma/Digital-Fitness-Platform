5337607e166deb7dacab24f6ecff0afc5dcd2069
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-17 11:35:38 +0530

    Fixed Contact Page Frontend (CSS and JS)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,39 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.mainLayout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fitness Hub - Contact Us</title>
-    <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
-</head>
+@section('title', 'Contact')
 
-<body>
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+@endpush
 
-    <header>
-        <div class="container">
-            <ul>
-                <li><a href="{{ route('home') }}" class="logo">
-                        <h2>Fitness Hub</h2>
-                    </a></li>
-                <li><a href="{{ route('home') }}" class="nav-link">Home</a></li>
-                <li><a href="{{ route('shop') }}" class="nav-link">Shop</a></li>
-            </ul>
-        </div>
-    </header>
+@section('content')
+<main>
+    <section class="contact">
+        <div class="contact-container">
+            <div class="left">
+                <div class="form-wrapper">
+                    <div class="contact-heading">
+                        <h1>Let's work together<span>.</span></h1>
+                        <p class="text">Or reach us via : <a href="#">FitnessGroup777@gmail.com</a></p>
+                    </div>
 
-    <main>
-        <section class="contact">
-            <div class="container">
-                <div class="left">
-                    <div class="form-wrapper">
-                        <div class="contact-heading">
-                            <h1>Let's work together<span>.</span></h1>
-                            <p class="text">Or reach us via : <a href="#">FitnessGroup777@gmail.com</a></p>
-                        </div>
-
-                        <form action="{{ route("contact.store") }}" method="post" class="contact-form">
-                            @csrf
+                    <form action="{{ route("contact.store") }}" method="post">
+                        @csrf
+                        <div class="contact-form">
                             <div class="input-wrap w-100">
                                 <input type="text" class="contact-input" autocomplete="off" name="name" required>
                                 <label>Full Name</label>
@@ -42,8 +28,7 @@
                             @error('name')
                                 <div class="error">{{ $message }}</div>
                             @enderror
-                            
-
+    
                             <div class="input-wrap w-100">
                                 <input type="number" class="contact-input" autocomplete="off" name="phone"
                                     pattern="[+]?[0-9]{1,4}[0-9]{10}" required>
@@ -53,7 +38,7 @@
                             @error('phone')
                                 <div class="error">{{ $message }}</div>
                             @enderror
-
+    
                             <div class="input-wrap w-100">
                                 <input type="email" class="contact-input" autocomplete="off" name="email" required>
                                 <label>Email</label>
@@ -62,7 +47,7 @@
                             @error('email')
                                 <div class="error">{{ $message }}</div>
                             @enderror
-
+    
                             <div class="input-wrap textarea w-100">
                                 <textarea name="message" class="contact-input" autocomplete="off" required></textarea>
                                 <label>Message</label>
@@ -71,35 +56,36 @@
                             @error('message')
                                 <div class="error">{{ $message }}</div>
                             @enderror
-                            <div class="contact-buttons">
-                                <input type="submit" value="Send message" class="btn" style="width: 100%;">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="right">
-                    <div class="image-wrapper">
-                        <img src="./images/bg1.jpeg" alt="background image" class="img">
-                        <div class="wave-wrap">
-                            <svg class="wave" viewBox="0 0 783 1536" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path id="wave"
-                                    d="M236.705 1356.18C200.542 1483.72 64.5004 1528.54 1 1535V1H770.538C793.858 63.1213 797.23 196.197 624.165 231.531C407.833 275.698 274.374 331.715 450.884 568.709C627.393 805.704 510.079 815.399 347.561 939.282C185.043 1063.17 281.908 1196.74 236.705 1356.18Z"
-                                    fill="#D9D9D9" stroke="white" />
-                            </svg>
                         </div>
-                        <svg class="dashed-wave" viewBox="0 0 345 877" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path id="dashed-wave"
-                                d="M0.5 876C25.6667 836.167 73.2 739.8 62 673C48 589.5 35.5 499.5 125.5 462C215.5 424.5 150 365 87 333.5C24 302 44 237.5 125.5 213.5C207 189.5 307 138.5 246 87C185 35.5 297 1 344.5 1"
-                                stroke="white" />
-                        </svg>
-                    </div>
+                        <div class="contact-buttons">
+                            <input type="submit" value="Send message" class="btn" style="width: 100%;">
+                        </div>
+                    </form>
                 </div>
             </div>
-        </section>
-    </main>
+            <div class="right">
+                <div class="image-wrapper">
+                    <img src="./images/bg1.jpeg" alt="background image" class="img">
+                    <div class="wave-wrap">
+                        <svg class="wave" viewBox="0 0 783 1536" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path id="wave"
+                                d="M236.705 1356.18C200.542 1483.72 64.5004 1528.54 1 1535V1H770.538C793.858 63.1213 797.23 196.197 624.165 231.531C407.833 275.698 274.374 331.715 450.884 568.709C627.393 805.704 510.079 815.399 347.561 939.282C185.043 1063.17 281.908 1196.74 236.705 1356.18Z"
+                                fill="#D9D9D9" stroke="white" />
+                        </svg>
+                    </div>
+                    <svg class="dashed-wave" viewBox="0 0 345 877" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path id="dashed-wave"
+                            d="M0.5 876C25.6667 836.167 73.2 739.8 62 673C48 589.5 35.5 499.5 125.5 462C215.5 424.5 150 365 87 333.5C24 302 44 237.5 125.5 213.5C207 189.5 307 138.5 246 87C185 35.5 297 1 344.5 1"
+                            stroke="white" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+@endsection
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
-    <script src="{{ asset('css/contact.css') }}"></script>
-</body>
-
-</html>
+@push('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
+<script src="{{ asset('js/contact.js') }}"></script>
+@endpush
