c95594c73ad422709e647f78b0def0ab0ff8551e
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-22 12:23:22 +0530

    Added favicon link

--- resources/views/layouts/formLayout.blade.php
+++ resources/views/layouts/formLayout.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- <link rel="icon" type="image/png" href="../assets/img/admin.png"> -->
+  <link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon.png') }}">
   <title>@yield('title')</title>
   <!-- Fonts and icons -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
