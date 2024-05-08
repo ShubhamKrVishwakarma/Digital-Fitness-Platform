5fe8bcf57370f897ab77fcf86d6539007f62104e
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-05-08 19:22:26 +0530

    Fixed Orders and Bill Page

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -7,72 +7,97 @@
 @endpush
 
 @section("content")
-    <!-- Orders Section -->
-    <div class="py-3 py-md-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="shadow bg-white p-3">
-                        <h4 class="mb-4"> My Orders</h4>
-                        <hr>
-                        <div class="table-responsive" style="min-height: 300px">
-                            <table class="table tble-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        {{-- <th class="text-center fw-semibold">Id</th> --}}
-                                        <th class="text-center fw-semibold">Date</th>
-                                        <th class="text-center fw-semibold">Total Products</th>
-                                        {{-- <th class="text-center fw-semibold">Role</th> --}}
-                                        <th class="text-center fw-semibold">Status</th>
-                                        <th class="text-center fw-semibold">Amount</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                    <tbody>
-                                        @forelse ($orders as $order)
-                                           <tr>
-                                               <td class="text-center">
-                                                   <span>{{ $order->created_at->format('d-m-Y') }}</span>
-                                               </td>   
-                                                <td class="text-center">
-                                                    <span>{{ $order->orders->count() }}</span>
-                                                </td>    
-                                                <td class="text-center">
-                                                    <span class="badge text-bg-{{ ($order->status === "confirmed") ? 'success' : ($order->status === "rejected" ? 'danger' : 'info') }} text-white">{{ $order->status }}</span>
-                                                </td>    
-                                                <td class="text-center">
-                                                    <span><i class="bi bi-currency-rupee"></i> {{ $order->amount }}</span>
-                                                </td>    
-                                                <td class="text-center">
-                                                    <a href="{{ route('bill', $order->id) }}" class="btn btn-primary btn-sm">View Bill</a>
-                                                </td>    
-                                            </tr> 
-                                        @empty
-                                            <tr>
-                                                <td class="text-center" colspan="7">No Orders Available</td>
-                                            </tr>
-                                        @endforelse
-                                    </tbody>
-                            </table>
-                        </div>
+<!-- Orders Section -->
+<div class="py-3 py-md-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="shadow bg-white p-3">
+                    <h4 class="mb-4"> My Orders</h4>
+                    <hr>
+                    <div class="table-responsive" style="min-height: 300px">
+                        <table class="table tble-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th class="text-center fw-semibold">Date</th>
+                                    <th class="text-center fw-semibold">Amount</th>
+                                    <th class="text-center fw-semibold">Status</th>
+                                    <th class="text-center fw-semibold">Payment</th>
+                                    <th class="text-center fw-semibold">Bill</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($orders as $order)
+                                <tr>
+                                    <td>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <span>{{ $order->created_at->format('d-m-Y') }}</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <span><i class="bi bi-currency-rupee"></i> {{ $order->amount }}</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <span class="badge text-bg-{{ ($order->status === " confirmed") ? 'success'
+                                                : ($order->status === "rejected" ? 'danger' : ($order->status === 'cancelled' ? 'danger' : 'info')) }} text-white">{{
+                                                $order->status }}</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <span>{{ $order->payment_mode }}</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <a href="{{ route('bill', $order->id) }}"
+                                                class="btn btn-primary btn-sm">View</a>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            @if ($order->status === "pending")
+                                                <form action="{{ route('cancel.order', $order->id) }}" method="POST">
+                                                    @method("DELETE")
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-sm btn-danger m-0">Cancel Order</button>
+                                                </form>
+                                            @else
+                                                <button type="submit" class="btn btn-sm btn-secondary m-0" disabled>{{ $order->status }}</button>
+                                            @endif
+                                        </div>
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr>
+                                    <td class="text-center" colspan="7">No Orders Available</td>
+                                </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push("scripts")
 <script src="{{ asset('js/orders.js') }}"></script>
 @if(session('alert'))
-  <script>
-      Swal.fire({
+<script>
+    Swal.fire({
         position: "top",
         icon: "success",
         title: "{{ session('alert') }}",
         showConfirmButton: false,
         timer: 1200
       });
-  </script>
+</script>
 @endif
 @endpush
